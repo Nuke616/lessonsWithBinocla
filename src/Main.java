faa5be049f8f7c9056b273b57bb5c5c94acea2d8
Nuke616 <146358016+Nuke616@users.noreply.github.com>
2026-03-17 15:50:40 +0300

    Правки после замечаний

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,31 @@
 import java.util.Scanner;
 
- //* Input: N - строка, содержащее ваше имя и следующий шаблонный текст:
-        //" says that Binocla loves you!". Так, например, если Иван решит передать послание,
-//он введет в консоль "Ivan says that Binocla loves you!". Имя гарантированно состоит из одного слова.
-//До имени гарантированно нет пробелов. Если шаблон нарушен (например, не хватает пробела/неправильный регистр/
-      //  неправильный символ/отсутствие или замена восклицательного знака) - вывести "What a mess!" (без кавычек)
-
-
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int i2 = 0;
-        while (i2 ==0) {
-            System.out.println("Введи имя");
+
+        while (true) {
+            System.out.println("введи имя и послание согласно шаблону ниже");
+            System.out.println("YourName says that Binocla loves you!");
+
             String input = scanner.nextLine();
-            if (input.substring(0, 1).contains(" ") || input.charAt(input.length() - 1) != ' ') {
-                System.out.println("What a mess!");
-            } else {
-                System.out.println("Введи шаблонную фразу");
-                String halfletter = scanner.nextLine(); // "says that Binocla loves you!";
-                if (!halfletter.equals("says that Binocla loves you!")) {
-                    System.out.println("What a mess!");
-                } else {
-                    String fullLetter = input + halfletter;
-                    int lovelettersum = 0;
-                    for (int i = 0; i < fullLetter.length(); i++) {
-                        lovelettersum += fullLetter.charAt(i);
-                    }
-                    System.out.println(lovelettersum);
-                    i2 ++;
+            String[] words = input.split(" ");
+            if (words.length == 6 &&
+                    !words[0].startsWith(" ") && !words[0].endsWith(" ") &&
+                    words[1].equals("says") &&
+                    words[2].equals("that") &&
+                    words[3].equals("Binocla") &&
+                    words[4].equals("loves") &&
+                    words[5].equals("you!")) {
+
+                int summa = 0;
+                for (int i = 0; i < input.length(); i++) {
+                    summa += input.charAt(i);
                 }
+                System.out.println(summa);
+            } else {
+                System.out.println("What a mess!");
             }
         }
     }
 }
-

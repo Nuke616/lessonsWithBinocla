b2050acbefe06d50b1740a601115b0789eb50e0d
Nuke616 <146358016+Nuke616@users.noreply.github.com>
2026-03-17 12:52:25 +0300

    комит второго задания на проверку идеи как считать цифры

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,36 @@
 import java.util.Scanner;
 
+ //* Input: N - строка, содержащее ваше имя и следующий шаблонный текст:
+        //" says that Binocla loves you!". Так, например, если Иван решит передать послание,
+//он введет в консоль "Ivan says that Binocla loves you!". Имя гарантированно состоит из одного слова.
+//До имени гарантированно нет пробелов. Если шаблон нарушен (например, не хватает пробела/неправильный регистр/
+      //  неправильный символ/отсутствие или замена восклицательного знака) - вывести "What a mess!" (без кавычек)
+
+
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
-        String halfletter = " says that Binocla loves you!";
-        if (input.isEmpty() || input.contains(" ")) {
-            System.out.println("What a mess!");
-        } else {
-            String fullletter = input.concat(halfletter);
-            int lovelettersum = 0;
-            for (int i = 0; i < fullletter.length(); i++) {
-                lovelettersum += fullletter.charAt(i);
+        int i2 = 0;
+        while (i2 ==0) {
+            System.out.println("Введи имя");
+            String input = scanner.nextLine();
+            if (input.substring(0, 1).contains(" ") || input.charAt(input.length() - 1) != ' ') {
+                System.out.println("What a mess!");
+            } else {
+                System.out.println("Введи шаблонную фразу");
+                String halfletter = scanner.nextLine(); // "says that Binocla loves you!";
+                if (!halfletter.equals("says that Binocla loves you!")) {
+                    System.out.println("What a mess!");
+                } else {
+                    String fullLetter = input + halfletter;
+                    int lovelettersum = 0;
+                    for (int i = 0; i < fullLetter.length(); i++) {
+                        lovelettersum += fullLetter.charAt(i);
+                    }
+                    System.out.println(lovelettersum);
+                    i2 ++;
+                }
             }
-            System.out.println(lovelettersum);
         }
     }
 }

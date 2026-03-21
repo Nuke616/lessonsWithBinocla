78137f3779a6794feba6740f089c87ac03be89fb
Nuke616 <146358016+Nuke616@users.noreply.github.com>
2026-03-21 11:11:00 +0300

    Правки после замечаний

---
+++ src/lesson4.java
@@ -0,0 +1,24 @@
+
+/*
+Дается строка:
+name=Sergey;age=23;city=Kazan
+
+Нужно:
+Вывести в формате:
+Name: Sergey
+Age: 23
+City: Kazan
+mainstring[i].substring(0,1).toUpperCase()+mainstring[i].substring(0,1).toUpperCase()+
+Важно! Порядок может быть любой. Типо city=Kazan;name=Sergey;age=23
+Давай пока сделаем с помощью строк
+*/
+public class lesson4 {
+    public static void main(String[] args) {
+        String[] mainstring = "name=Sergey;age=23;city=Kazan".replace("=",":").replace(";"," ").split(" ");
+        String finalstring="";
+        for (int i =0; i < mainstring.length;i++){
+            finalstring= mainstring[i].substring(0,1).toUpperCase().concat(mainstring[i].substring(1));
+            System.out.println(finalstring);
+        }
+    }
+}
